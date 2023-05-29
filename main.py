020330d79d26f4ac9e507dc4c50977cc87ff8a08
FuaadNew <134631945+FuaadNew@users.noreply.github.com>
2023-05-29 18:09:46 +0000

    Comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #Number Guessing Game Objectives:
 
-# Include an ASCII art logo.
 # Allow the player to submit a guess for a number between 1 and 100.
 EndGame = False
 import random
@@ -13,7 +12,6 @@ print(""" / _ \_   _  ___  ___ ___  /__   \ |__   ___    /\ \ \_   _ _ __ ___ | 
 print("Welcome to the Number Guessing Game!")
 numberlist = list(range(1,101))
 
-#def game():
 print("I'm thinking of a number between 1 and 100.")
 Answer = random.choice(numberlist)
 print(f"Pssst, the correct answer is {Answer}")
@@ -49,8 +47,3 @@ while EndGame == False:
   
   
   
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-## Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode)
